06f9ea6dea6e182bc22e398a2ac9e5a80126d86a
Anton Schins <aschins@gmail.com>
2021-10-19 11:54:09 +0200

    in rooster zit een foto upload gedeelte

--- lib/src/rooster.dart
+++ lib/src/rooster.dart
@@ -1,23 +1,80 @@
+import 'package:firebase_picture_uploader/firebase_picture_uploader.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+//import 'package:firebase_core/firebase_core.dart';
+
+//void main() async {
+//  WidgetsFlutterBinding.ensureInitialized();
+//  await Firebase.initializeApp();
+//  runApp(const ExampleApp());
+//}
 
 class Rooster extends StatelessWidget {
+  const Rooster({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Mooi rooster zeg'),
-        //style: Theme.of(context).textTheme.headline1),
-//        actions: [
-//          IconButton(
-//            icon: Icon(Icons.arrow_forward_outlined),
-//            onPressed: () => Navigator.pushNamed(context, '/cart'),
-//          ),
-//        ],
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  const MyHome({Key? key}) : super(key: key);
+
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
+  List<UploadJob>? _profilePictures = [];
+
+  @override
+  Widget build(BuildContext context) {
+    final profilePictureTile = Material(
+      color: Colors.transparent,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Text('Profile Picture',
+              style: TextStyle(
+                color: CupertinoColors.systemBlue,
+                fontSize: 15.0,
+              )),
+          const Padding(
+            padding: EdgeInsets.only(bottom: 5.0),
+          ),
+          PictureUploadWidget(
+            initialImages: _profilePictures,
+            onPicturesChange: profilePictureCallback,
+            buttonStyle: PictureUploadButtonStyle(),
+            buttonText: 'Upload Picture',
+            localization: PictureUploadLocalization(),
+            settings: PictureUploadSettings(
+              // customDeleteFunction: ProfileController.deleteProfilePicture,
+              // customUploadFunction: RecipeController.uploadRecipePicture,
+                imageSource: ImageSourceExtended.askUser,
+                minImageCount: 0,
+                maxImageCount: 5,
+                imageManipulationSettings: const ImageManipulationSettings(
+                    enableCropping: true, compressQuality: 75)),
+            enabled: true,
+          ),
+        ],
       ),
-      body: Container(
-        decoration: new BoxDecoration(color: Colors.orange[300]),
-        child: Text('hier moet rooster komen'),
-        ),
-      );
+    );
+
+    return Scaffold(
+      body: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 100, 20, 50),
+          child: Column(children: <Widget>[profilePictureTile])),
+    );
+  }
+
+  void profilePictureCallback(
+      {List<UploadJob>? uploadJobs, bool? pictureUploadProcessing}) {
+    _profilePictures = uploadJobs;
   }
 }
